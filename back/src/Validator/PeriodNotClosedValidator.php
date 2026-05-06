f2a696e4106db6c2d90cbeadc859e95a1f48b919
andreas2104 <andrianiainaandreas99@gmail.com>
2026-05-06 21:22:11 +0300

    feat(period): add period entity with date range and school year relation

---
+++ back/src/Validator/PeriodNotClosedValidator.php
@@ -0,0 +1,9 @@
+<?php
+
+// public function validate($note, Constraint $constraint)
+// {
+//   if ($note->getPeriods()->getStatus() === 'closed') {
+//     $this->$context->buildViolation("Imposible to change note on  block period")
+//     ->addvalidation();
+//   }
+// }
